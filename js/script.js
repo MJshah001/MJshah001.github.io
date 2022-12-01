587fde57eba47ff10bfaa4cc605efcac068c5024
Monil Shah <70857971+MJshah001@users.noreply.github.com>
2022-12-01 15:32:32 +0530

    raw structure
    end of constructing phase1
    informations yet to be updated

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* ============================ typing text animation =========================== */
+
+var typed = new Typed(".typing", {
+    strings:[" ","An Aspiring Data Scientist","Data Analyst","Web developer"],
+    typeSpeed : 100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+
