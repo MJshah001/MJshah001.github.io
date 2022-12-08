07d3cfc90cba4bc92c9ca1b56d747159b5aa0217
Monil Shah <70857971+MJshah001@users.noreply.github.com>
2022-12-08 11:25:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* ============================ typing text animation =========================== */
 
 var typed = new Typed(".typing", {
-    strings:[" ","Software Developer","Data Analyst","Python developer"],
+    strings:[" ","Data Analyst","Python developer"],
     typeSpeed : 100,
     BackSpeed:60,  
     loop:true
